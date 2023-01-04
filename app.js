06a55f7226d037d701c8089f4040deba6a35dfeb
alihuzaifa <ha4587323@gmail.com>
2023-01-04 21:47:58 +0500

    updating some todo-application features

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// ======================================  Firebase  ===================================== 
-import { app } from "./firebase/firebase-config.js"
+// ======================================  Firebase  =====================================
+import { app } from "./firebase/firebase-config.js";
 import {
-    getAuth,
-    signInWithEmailAndPassword,
-    onAuthStateChanged
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
 
-// ======================================  All variables  ===================================== 
+// ======================================  All variables  =====================================
 const auth = getAuth(app);
 const email = document.querySelector("#inputEmail");
 const password = document.querySelector("#inputPassword");
@@ -14,46 +14,54 @@ const login = document.querySelector("#login");
 const emailError = document.querySelector(".email-error");
 const passwordError = document.querySelector(".password-error");
 const mainError = document.querySelector(".main-error");
+const buttton = document.querySelector(".button");
 
 // ======================================  Main Function  =====================================
 const loginData = async () => {
-    if (email.value !== "" || password.value !== "") {
-        try {
-            let user = await signInWithEmailAndPassword(auth, email.value, password.value)
-            login.innerHTML = "Sign In"
-            location = `signup/forget/verification/Todos/todo.html`
-        }
-        catch (error) {
-            if (error.message === `Firebase: Error (auth/user-not-found).`) {
-                mainError.innerHTML = "You don't have any account."
-                mainError.classList.add("error");
-                mainError.style.display = "block";
-                childClearError()
-            } else if (error.message === `Firebase: Error (auth/wrong-password).`) {
-                mainError.innerHTML = `Wrong Password.`
-                mainError.classList.add("error");
-                mainError.style.display = "block";
-                childClearError()
-            }
-        }
-    } else {
-        mainError.innerHTML = "Please Fill All Input Fields."
+  if (email.value !== "" || password.value !== "") {
+    try {
+      buttton.innerHTML = `<div class="spinner-border"></div>`;
+      let user = await signInWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      );
+      buttton.innerHTML = "Sign In";
+      location = `signup/forget/verification/Todos/todo.html`;
+    } catch (error) {
+      if (error.message === `Firebase: Error (auth/user-not-found).`) {
+        mainError.innerHTML = "You don't have any account.";
         mainError.classList.add("error");
         mainError.style.display = "block";
-        childClearError()
+        buttton.innerHTML = "Sign In";
+        childClearError();
+      } else if (error.message === `Firebase: Error (auth/wrong-password).`) {
+        mainError.innerHTML = `Wrong Password.`;
+        mainError.classList.add("error");
+        mainError.style.display = "block";
+        buttton.innerHTML = "Sign In";
+        childClearError();
+      }
     }
-}
+  } else {
+    mainError.innerHTML = "Please Fill All Input Fields.";
+    mainError.classList.add("error");
+    mainError.style.display = "block";
+    buttton.innerHTML = "Sign In";
+    childClearError();
+  }
+};
 login.addEventListener("click", loginData);
 
 // ======================================  Clear All error after 4 seconds  =====================================
 function clearAllError() {
-    emailError.innerHTML = "";
-    passwordError.innerHTML = "";
-    mainError.innerHTML = "";
-    emailError.style.display = "none";
-    passwordError.style.display = "none";
-    mainError.style.display = "none";
+  emailError.innerHTML = "";
+  passwordError.innerHTML = "";
+  mainError.innerHTML = "";
+  emailError.style.display = "none";
+  passwordError.style.display = "none";
+  mainError.style.display = "none";
 }
 function childClearError() {
-    setTimeout(clearAllError, 4000);
+  setTimeout(clearAllError, 6000);
 }
--- signup/forget/verification/verify.js
+++ signup/forget/verification/verify.js
@@ -1,62 +1,70 @@
-// ======================================  Firebase  ===================================== 
+// ======================================  Firebase  =====================================
 import { auth } from "../../../firebase/firebase-config.js";
 import {
-    sendEmailVerification,
-    onAuthStateChanged
+  sendEmailVerification,
+  onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
 
-// ======================================  All Variables  ===================================== 
+// ======================================  All Variables  =====================================
 const resendBtn = document.querySelector("#resend");
 const verified = document.querySelector("#verify");
 const verifyError = document.querySelector(".error");
 let getUser;
 
-// // ======================================  Resend Email to user Gmail  ===================================== 
-function resend(){
-    sendEmailVerification(auth.currentUser)
+// // ======================================  Resend Email to user Gmail  =====================================
+function resend() {
+  event.preventDefault();
+  resendBtn.innerHTML = `<div class="spinner-border"></div>`;
+  sendEmailVerification(auth.currentUser);
+  resendBtn.innerHTML = `Resend`;
+  swal({
+    title: "Good!",
+    text: "We have send you confirmation email. Check it",
+    icon: "success",
+    button: "OK",
+  });
 }
-resendBtn.addEventListener("click",resend)
+resendBtn.addEventListener("click", resend);
 
-// // ======================================  You have to define it first  ===================================== 
-function checkVerification(){
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-          const uid = user.uid;
-          getUser = user
-          changedPage()
-        }
-         else {
-          console.log("User is signed out")
-        }
-      });
+// // ======================================  You have to define it first  =====================================
+function checkVerification() {
+  verified.innerHTML = `<div class="spinner-border"></div>`;
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      getUser = user;
+      changedPage();
+    } else {
+      console.log("User is signed out");
+    }
+  });
+  verified.innerHTML = `Verified`;
 }
-verified.addEventListener("click",checkVerification);
+verified.addEventListener("click", checkVerification);
 
-// // ======================================  Check Verification  ===================================== 
-function changedPage(){
-    if(getUser.emailVerified === true){
-        swal({
-            title: "Good!",
-            text: "You have successfully subscribe to your account now login again!",
-            icon: "success",
-            button: "OK",
-          });
-        
-    }
-    else{
-        verifyError.innerHTML = "Please check your email and confirmed";
-        verifyError.classList.add("error");
-        verifyError.style.display = "block";
-        verifyError.innerHTML = "Please check your email and confirmed.";
-        removeError();
-    }
+// // ======================================  Check Verification  =====================================
+function changedPage() {
+  event.preventDefault();
+  if (getUser.emailVerified === true) {
+    swal({
+      title: "Good!",
+      text: "You have successfully subscribe to your account now login again!",
+      icon: "success",
+      button: "OK",
+    });
+  } else {
+    verifyError.innerHTML = "Please check your email and confirmed";
+    verifyError.classList.add("error");
+    verifyError.style.display = "block";
+    verifyError.innerHTML = "Please check your email and confirmed.";
+    removeError();
+  }
 }
 
 const clear = () => {
-    verifyError.innerHTML = ``;
-    verifyError.style.display = "none";
-
-}
-function removeError(){
-    setTimeout(clear,4000)
+  verifyError.innerHTML = ``;
+  verifyError.style.display = "none";
+};
+function removeError() {
+  setTimeout(clear, 5000);
 }
